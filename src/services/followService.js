c717339d43b0a9349e9bf4a08d00c0fbac48f400
Gabriela Araujo <gabyara237@gmail.com>
2026-01-26 22:43:14 -0800

    feat: add follow user service

---
+++ src/services/followService.js
@@ -0,0 +1,26 @@
+const BASE_URL =`${import.meta.env.VITE_BACK_END_SERVER_URL}/users`;
+
+const followUser = async (userId) =>{
+    try{
+        const res = await fetch(`${BASE_URL}/${userId}/follow`,{
+            method: 'POST',
+            headers:{
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        if(!res.ok){
+            const errorData = await res.json();
+            throw new Error(errorData.err || 'Failed to follow user')
+        }
+        return res.json()
+    }catch(err){
+        console.log(err)
+        throw err;
+    }
+    
+}
+
+export{
+    followUser
+}
